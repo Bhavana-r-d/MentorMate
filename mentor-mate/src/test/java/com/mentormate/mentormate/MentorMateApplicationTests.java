36fcc895ac4af0c0215718924f5bf611169deb4d
Bhavana-r-d <bbhavanard@gmail.com>
2023-12-01 11:21:51 +0530

    adding Spring Initializer

---
+++ mentor-mate/src/test/java/com/mentormate/mentormate/MentorMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentormate.mentormate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
