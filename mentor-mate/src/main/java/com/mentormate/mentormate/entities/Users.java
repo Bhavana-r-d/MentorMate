6d70437218bdbded3b1e6d82151276e571ead2db
Chandana N <chandana.n@valtech.com>
2023-12-06 15:35:54 +0530

    ahh

--- mentor-mate/src/main/java/com/mentormate/mentormate/entities/Users.java
+++ mentor-mate/src/main/java/com/mentormate/mentormate/entities/Users.java
@@ -14,13 +14,55 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String email;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String password;
     private String role;
     private String designation;
 	public Users() {
 	}
 	
+	public Users(String email, String firstName, String lastName, String password, String role, String designation) {
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.role = role;
+		this.designation = designation;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	public String getDesignation() {
 		return designation;
 	}
@@ -33,26 +75,6 @@ public class Users {
 		this.id = userId;
 	}
 
-	public Users( String email, String name, String password, String role, String designation) {
-		this.email = email;
-		this.name = name;
-		this.password = password;
-		this.role = role;
-		this.designation = designation;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	public String getPassword() {
 		return password;
 	}
